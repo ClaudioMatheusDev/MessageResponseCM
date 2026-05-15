0b9dad37553b4120b134103f91a4a9ea91b5d8f1
Claudio Matheus <claudiomatheus055@gmail.com>
2026-05-14 22:41:04 -0300

    fix: Retirando conversão de NULL de tipagens GUID e int

--- NotificationSystemCM/src/NotificationContracts/Messages.cs
+++ NotificationSystemCM/src/NotificationContracts/Messages.cs
@@ -1,24 +1,24 @@
-﻿namespace NotificationContracts;
+﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace NotificationContracts;
 
 public class UsuarioCadastradoEvent
 {
-    public Guid UsuarioId { get; set; } = null!;
-    public string Nome { get; set; } = null!;
-    public string SobreNome { get; set; } = null!;
-    public DateTimeOffset DataCadastro { get; set; } = null!;
-    public string Email { get; set; } = null!;
+    public Guid UsuarioId { get; set; }
+    public string Nome { get; set; }
+    public DateTime DataCadastro { get; set; }
+    public string Email { get; set; }
 
 }
 
 public class PedidoConfirmadoEvent
 {
-    public Guid PedidoId { get; set; } = null!;
-    public Guid UsuarioId { get; set; } = null!;
+    public Guid PedidoId { get; set; }
     public string Nome { get; set; } = null!;
     public string NomePedido { get; set; } = null!;
     public string Email { get; set; } = null!;
     public string DescricaoPedido { get; set; } = null!;
-    public DateTimeOffset DataConfirmacao { get; set; } = null!;
-    public int VersaoEvento { get; set; } = null!;
+    public DateTime DataConfirmacao { get; set; }
+    public int VersaoEvento { get; set; }
 
 }
